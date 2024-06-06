87634049d0689df03bf604025e68fc2493b04a40
Filipe C Menezes <fcmenezes87@gmail.com>
2024-06-06 14:46:24 +0100

    fix index

--- langchain-index/main.py
+++ langchain-index/main.py
@@ -1,95 +1,53 @@
-from typing import Any, List
+from typing import List, Iterable
 import os
 import uuid
 import json
 
 import ollama
 from langchain_core.documents import Document
-from langchain_community.document_loaders import BSHTMLLoader
-from langchain_community.document_transformers import Html2TextTransformer
-from langchain_text_splitters import MarkdownTextSplitter
+from langchain_community.document_loaders import UnstructuredHTMLLoader, DirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.embeddings import Embeddings
-from langchain_community.embeddings import OllamaEmbeddings
 from langchain_core.vectorstores import VectorStore
+from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import FAISS
 
 EMBEDDING_MODEL = "mxbai-embed-large"
 
 
-def chunks(lst: List[str], n: int) -> List[List[str]]:
-    res = []
-    for i in range(0, len(lst), n):
-        res += [lst[i:i + n]]
-    return res
+def update_metadata(docs: Iterable[Document]) -> Iterable[Document]:
+    for doc in docs:
+        doc.metadata['local_path'] = doc.metadata['source']
+        metadata_path = doc.metadata['local_path'].removesuffix(
+            '.html') + '_metadata.json'
+        with open(metadata_path, 'r') as f:
+            file_metadata = json.load(f)
+            doc.metadata['source'] = file_metadata['url']
+        yield doc
 
 
-def split_load() -> List[List[str]]:
-    files = []
-    for (dirpath, dirnames, filenames) in os.walk('./data/docs'):
-        files.extend([os.path.join(dirpath, file)
-                     for file in filenames if file.endswith('.html')])
-    return chunks(files, 100)
+def load_docs(paths: tuple[str, ...]) -> Iterable[Document]:
+    for path in paths:
+        loader = DirectoryLoader(
+            path, "*.html", loader_cls=UnstructuredHTMLLoader)
+        for doc in loader.load():
+            yield doc
 
 
-def load_docs(files: List[str]) -> List[Document]:
-    print('Loading docs...')
-    l = [BSHTMLLoader(f).load() for f in files]
-    return [
-        entry
-        for subl in l
-        for entry in subl
-    ]
-
-
-def html_to_markdown(docs: List[Document]) -> List[Document]:
-    print('Converting markup to markdown...')
-    html2text = Html2TextTransformer()
-    return html2text.transform_documents(docs)
-
-
-def split_docs(docs: List[Document]) -> List[Document]:
-    print('Splitting markdown...')
-    text_splitter = MarkdownTextSplitter()
-    return text_splitter.split_documents(docs)
+def split_docs(docs: Iterable[Document]) -> Iterable[Document]:
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=10)
+    for splitted_doc in text_splitter.split_documents(docs):
+        yield splitted_doc
 
 
 def embeddings() -> Embeddings:
     return OllamaEmbeddings(model=EMBEDDING_MODEL)
 
 
-def setup_batch(id: str, files: List[str]) -> str:
-    dest = f'./data/langchain_index/chunks/{id}'
-
-    if os.path.exists(dest):
-        return dest
-
-    docs = split_docs(html_to_markdown(load_docs(files)))
-    print('Embedding...')
-    vs = FAISS.from_documents(docs, embeddings())
-    os.makedirs(os.path.dirname(dest), exist_ok=True)
-    vs.save_local(dest)
-    return dest
-
-
 def setup_docs() -> VectorStore:
-    map_path = './data/langchain_index/chunks/map.json'
-    map = None
-    if os.path.exists(map_path):
-        with open('./data/langchain_index/chunks/map.json', 'r') as f:
-            map = json.load(f)
-    else:
-        with open('./data/langchain_index/chunks/map.json', 'w') as f:
-            batches = split_load()
-            map = [{'id': str(uuid.uuid4()), 'files': batch} for batch in batches]
-            json.dump(map, f)
-    paths = [setup_batch(batch['id'], batch['files']) for batch in map]
-    vs = FAISS.load_local(paths[0], embeddings(),
-                          allow_dangerous_deserialization=True)
-    for path in paths[1:]:
-        current_vs = FAISS.load_local(path, embeddings(),
-                                      allow_dangerous_deserialization=True)
-        vs.merge_from(current_vs)
-    return vs
+    docs = list(split_docs(update_metadata(
+        load_docs(('./data/docs/atlas/atlas-cli', './data/docs/atlas/cli')))))
+    return FAISS.from_documents(docs, embeddings())
 
 
 def main() -> None:
