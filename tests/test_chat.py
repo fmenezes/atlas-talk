e83a52231359b38da6157870aff55f47bebea08e
Filipe C Menezes <fcmenezes87@gmail.com>
2024-06-07 22:48:52 +0100

    Update tests

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -1,19 +1,28 @@
-import re
+import pytest
+import sys
+
 from atlas_talk.config import Config
-from atlas_talk.chat import invoke, setup
+from atlas_talk.chat import setup
+from llama_index.core.evaluation import RelevancyEvaluator, FaithfulnessEvaluator
 
-def test_rag():
-    config = Config('test')
+prompts = ["How do I create a cluster?", "How to create an atlas search index?"]
+envs = ['openai', 'openai-gpt-4o', 'ollama']
+evaluators = [
+    FaithfulnessEvaluator,
+    RelevancyEvaluator,
+]
+query_env_evals = [(p, e, ev) for ev in evaluators for p in prompts for e in envs]
+
+
+@pytest.mark.parametrize('query,env,evaluator_cls', query_env_evals)
+def test_evaluation(query, env, evaluator_cls):
+    config = Config(env)
     engine = setup(config)
-    got = invoke(engine, "How do I create a cluster?")
-    expect = 'atlas clusters create'
-    assert re.search(expect, got), 'expected: ' + expect + ' got: ' + got
-
-
-def test_memory():
-    config = Config('test')
-    engine = setup(config)
-    invoke(engine, "hi I'm John, how are you?")
-    got = invoke(engine, "do you remmeber my name?")
-    expect = 'John'
-    assert re.search(expect, got), 'expected: ' + expect + ' got: ' + got
+    response = engine.chat(query)
+    evaluator = evaluator_cls()
+    result = evaluator.evaluate_response(
+        query=query, response=response)
+    if isinstance(result.passing, bool):
+        assert result.passing, result.feedback
+    else:
+        pytest.skip(result.feedback)
