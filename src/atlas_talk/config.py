c4ba38816ead83ed3da46696f962e3cf45faabfb
Filipe C Menezes <fcmenezes87@gmail.com>
2024-06-07 16:51:06 +0100

    refactor config

--- src/atlas_talk/config.py
+++ src/atlas_talk/config.py
@@ -38,46 +38,164 @@ class Config:
         Raises:
             RuntimeError: If the specified environment is not found or the .env file does not exist.
         """
-        self.env: str = env
-        self.env_file: Optional[str] = None
+        self._env: str = env
+        self._env_file: Optional[str] = None
 
         if env is not None and env.strip() != "":
             env_file = f"./env/{env}.env"
             if not os.path.exists(env_file):
                 raise RuntimeError(
-                    f"configuration {env} not found, file {env_file} must be created"
+                    f"configuration {env} not found, file {
+                        env_file} must be created"
                 )
-            self.env_file = env_file
+            self._env_file = env_file
 
         dotenv.load_dotenv(
-            self.env_file, override=True
+            self._env_file, override=True
         )  # loads .env, env/openai.env, env/ollama.env, etc
-        self.system_prompt: str = os.getenv(
+
+    @property
+    def system_prompt(self) -> str:
+        """
+        System prompt for the AI assistant.
+
+        Returns:
+            str: The system prompt.
+        """
+        return os.getenv(
             "SYSTEM_PROMPT",
-            ("You are MongoDB Atlas CLI Help Assistant, you know atlas cli command reference. "
-             "You should assume atlas cli is properly installed. When you don't know the answer "
-             "try looking up the information first and if you still can't find it say you don't know it, "
-             "do not try to make up an answer. Format your answers in Markdown."),
+            (
+                "You are MongoDB Atlas CLI Help Assistant, you know atlas cli command reference. "
+                "You should assume atlas cli is properly installed. When you don't know the answer "
+                "try looking up the information first and if you still can't find it say you don't know it, "
+                "do not try to make up an answer. Format your answers in Markdown."
+            ),
         )
-        self.index_path: str = os.getenv("INDEX_PATH", "./data/index")
-        self.collection_name: str = os.getenv("COLLECTION_NAME", "atlascli-commands")
-        self.ai_platform: str = os.getenv("AI_PLATFORM", "OPENAI")
-        self.openai_api_key: Optional[str] = os.getenv("OPENAI_API_KEY")
-        self.openai_model: str = os.getenv("OPENAI_MODEL", DEFAULT_OPENAI_MODEL)
-        self.openai_embed_model: OpenAIEmbeddingModeModel = OpenAIEmbeddingModeModel[
+
+    @property
+    def index_path(self) -> str:
+        """
+        Index path for the AI assistant.
+
+        Returns:
+            str: The index path.
+        """
+        return os.getenv("INDEX_PATH", "./data/index")
+
+    @property
+    def collection_name(self) -> str:
+        """
+        Collection name for the AI assistant.
+
+        Returns:
+            str: The collection name.
+        """
+        return os.getenv("COLLECTION_NAME", "atlascli-commands")
+
+    @property
+    def ai_platform(self) -> str:
+        """
+        AI platform for the AI assistant.
+
+        Returns:
+            str: The AI platform.
+        """
+        return os.getenv("AI_PLATFORM", "OPENAI")
+
+    @property
+    def openai_api_key(self) -> Optional[str]:
+        """
+        OpenAI API key for the AI assistant.
+
+        Returns:
+            Optional[str]: The OpenAI API key.
+        """
+        return os.getenv("OPENAI_API_KEY")
+
+    @property
+    def openai_model(self) -> str:
+        """
+        OpenAI model for the AI assistant.
+
+        Returns:
+            str: The OpenAI model.
+        """
+        return os.getenv("OPENAI_MODEL", DEFAULT_OPENAI_MODEL)
+
+    @property
+    def openai_embed_model(self) -> OpenAIEmbeddingModeModel:
+        """
+        OpenAI embedding model for the AI assistant.
+
+        Returns:
+            OpenAIEmbeddingModeModel: The OpenAI embedding model.
+        """
+        return OpenAIEmbeddingModeModel[
             os.getenv("OPENAI_EMBED_MODEL", OpenAIEmbeddingModeModel.TEXT_EMBED_ADA_002.name)
         ]
-        self.ollama_model: str = os.getenv("OLLAMA_MODEL", "mistral")
-        self.ollama_embed_model: str = os.getenv("OLLAMA_EMBED_MODEL", "nomic-embed-text")
-        self.llama_cpp_model_url: str = os.getenv(
-            "LLAMA_CPP_MODEL_URL", DEFAULT_LLAMA_CPP_GGUF_MODEL
-        )
+
+    @property
+    def ollama_model(self) -> str:
+        """
+        OLLAMA model for the AI assistant.
+
+        Returns:
+            str: The OLLAMA model.
+        """
+        return os.getenv("OLLAMA_MODEL", "mistral")
+
+    @property
+    def ollama_embed_model(self) -> str:
+        """
+        OLLAMA embedding model for the AI assistant.
+
+        Returns:
+            str: The OLLAMA embedding model.
+        """
+        return os.getenv("OLLAMA_EMBED_MODEL", "nomic-embed-text")
+
+    @property
+    def llama_cpp_model_url(self) -> str:
+        """
+        LLama CPP model URL for the AI assistant.
+
+        Returns:
+            str: The LLama CPP model URL.
+        """
+        return os.getenv("LLAMA_CPP_MODEL_URL", DEFAULT_LLAMA_CPP_GGUF_MODEL)
+
+    @property
+    def n_gpu_layers(self) -> int:
+        """
+        Number of GPU layers for the AI assistant.
+
+        Returns:
+            int: The number of GPU layers.
+        """
         try:
-            n_gpu_layers = int(os.getenv("N_GPU_LAYERS", "-1"))
+            return int(os.getenv("N_GPU_LAYERS", "-1"))
         except ValueError:
-            n_gpu_layers = -1
-        self.n_gpu_layers: int = n_gpu_layers
-        self.llama_cpp_embed_model: str = os.getenv(
-            "LLAMA_CPP_EMBED_MODEL", DEFAULT_HUGGINGFACE_EMBEDDING_MODEL
-        )
-        self.skip_rag: bool = os.getenv("SKIP_RAG", "False").lower() in ("true", "1", "t")
+            return -1
+
+    @property
+    def llama_cpp_embed_model(self) -> str:
+        """
+        LLama CPP embedding model for the AI assistant.
+
+        Returns:
+            str: The LLama CPP embedding model.
+        """
+        return os.getenv("LLAMA_CPP_EMBED_MODEL", DEFAULT_HUGGINGFACE_EMBEDDING_MODEL)
+
+    @property
+    def skip_rag(self) -> bool:
+        """
+        Flag to skip RAG for the AI assistant.
+
+        Returns:
+            bool: The flag.
+        """
+        try:
+            return os.getenv("SKIP_RAG", "False").lower() in ("true", "1", "t")
+        except ValueError:
+            return False
