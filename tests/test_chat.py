8c5a605962e95945754119674269da21485b8c76
Filipe C Menezes <fcmenezes87@gmail.com>
2024-06-10 11:34:25 +0100

    update tests

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -1,27 +1,52 @@
+from typing import List, Type
+import os
+
 import pytest
 
 from atlas_talk.config import Config
+from atlas_talk.settings import model
 from atlas_talk.chat import setup
 from llama_index.core.evaluation import RelevancyEvaluator, FaithfulnessEvaluator
+from llama_index.core.evaluation.base import BaseEvaluator
 
-prompts = ["How do I create a cluster?", "How to create an atlas search index?"]
-envs = ['openai', 'openai-gpt-4o', 'ollama']
-evaluators = [
-    FaithfulnessEvaluator,
-    RelevancyEvaluator,
-]
-query_env_evals = [(p, e, ev) for ev in evaluators for p in prompts for e in envs]
+def envs() -> List[str]:
+    files = os.listdir('./env')
+    envs = [os.path.basename(f).removesuffix('.env')
+             for f in files if f.endswith('.env')]
+    envs += [None]
+    return envs
 
 
-@pytest.mark.parametrize('query,env,evaluator_cls', query_env_evals)
-def test_evaluation(query, env, evaluator_cls):
-    config = Config(env)
-    engine = setup(config)
-    response = engine.chat(query)
-    evaluator = evaluator_cls()
-    result = evaluator.evaluate_response(
-        query=query, response=response)
-    if isinstance(result.passing, bool):
-        assert result.passing, result.feedback
-    else:
-        pytest.skip(result.feedback)
+@pytest.mark.parametrize(
+    "env",
+    envs(),
+)
+class TestEvaluation:
+    """Tests to run on each environment."""
+
+    @pytest.fixture
+    def prompts(self) -> List[str]:
+        """Prompts to be tested"""
+        return ["How do I create a cluster?",
+                "How to create an atlas search index?"]
+
+    def evaluate(self, evaluator_cls: Type[BaseEvaluator], env: str, prompts: List[str]) -> None:
+        model(Config(None))  # setup evaluator
+        evaluator = evaluator_cls()
+
+        config = Config(env)
+        engine = setup(config)
+        for query in prompts:
+            response = engine.chat(query)
+            result = evaluator.evaluate_response(
+                query=query, response=response)
+            if isinstance(result.passing, bool):
+                assert result.passing, result.feedback
+            else:
+                pytest.skip(result.feedback)
+
+    def test_faithfulness(self, env: str, prompts: List[str]) -> None:
+        self.evaluate(FaithfulnessEvaluator, env, prompts)
+
+    def test_relevancy(self, env: str, prompts: List[str]) -> None:
+        self.evaluate(RelevancyEvaluator, env, prompts)
